0b2e8d33fd80ac471fdd8588ccbbba76ec468507
zahra <zahra20hb@gmail.com>
2024-02-08 23:34:42 +0330

    icons added to menu

--- pages/components/Sidebar/index.jsx
+++ pages/components/Sidebar/index.jsx
@@ -1,43 +1,59 @@
 import Link from "next/link"
+import { RxDashboard } from "react-icons/rx";
+import { VscArchive } from "react-icons/vsc";
+import { RiListUnordered } from "react-icons/ri";
+import { GoNote } from "react-icons/go";
+import { HiOutlineUserGroup } from "react-icons/hi2";
+import { IoSettingsOutline } from "react-icons/io5";
+import { BiLogOut } from "react-icons/bi";
+
 
 const Sidebar = () => {
     return (
         <>
-            <div>
-                <ul>
+            <div className="flex flex-col items-start gap-5 bg-blue-900 w-1/5 h-screen p-10 text-white text-lg">
+                <h4 className="font-bold">Ecommerse Admin</h4>
+                <ul className="flex flex-col gap-5">
                     <li>
-                        <Link href={'/'}>
+                        <Link href={'/'} className="hover:text-gray-300 text-md flex items-center gap-3">
+                            <RxDashboard />
                             Dashboard
                         </Link>
                     </li>
                     <li>
-                        <Link href={'/'}>
+                        <Link href={'/'} className="hover:text-gray-300 text-md flex items-center gap-3">
+                            <VscArchive />
                             Products
                         </Link>
                     </li>
                     <li>
-                        <Link href={'/'}>
+                        <Link href={'/'} className="hover:text-gray-300 text-md flex items-center gap-3">
+                            <GoNote />
                             Orders
                         </Link>
                     </li>
                     <li>
-                        <Link href={'/'}>
+                        <Link href={'/'} className="hover:text-gray-300 text-md flex items-center gap-3">
+                            <RiListUnordered />
                             Categories
                         </Link>
                     </li>
                     <li>
-                        <Link href={'/'}>
+                        <Link href={'/'} className="hover:text-gray-300 text-md flex items-center gap-3">
+                            <HiOutlineUserGroup />
                             Admins
                         </Link>
                     </li>
                     <li>
-                        <Link href={'/'}>
+                        <Link href={'/'} className="hover:text-gray-300 text-md flex items-center gap-3">
+                        <IoSettingsOutline />
                             Settings
                         </Link>
                     </li>
                     <li>
-                        <Link href={'/'}>
-                            Signout
+                        <Link href={'/'} className="hover:text-gray-300 text-md flex items-center gap-3">
+                            <BiLogOut />
+                            Logout
                         </Link>
                     </li>
                 </ul>
