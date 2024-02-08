dbe6559aa73d2b8cf0ff9cff0783ec234aa4ed2c
zahra <zahra20hb@gmail.com>
2024-02-08 14:37:19 +0330

    login codes added

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,13 @@
+import GoogleProvider from 'next-auth/providers/google'
+import NextAuth from 'next-auth/next'
+
+export default NextAuth({
+    providers: [
+
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET
+        }),
+
+    ]
+})
