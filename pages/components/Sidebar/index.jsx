97e7aacc67c50129c6e7dfec06c7faadc9390b9e
zahra <zahra20hb@gmail.com>
2024-02-08 23:37:29 +0330

    styles for mednu added

--- pages/components/Sidebar/index.jsx
+++ pages/components/Sidebar/index.jsx
@@ -6,14 +6,16 @@ import { GoNote } from "react-icons/go";
 import { HiOutlineUserGroup } from "react-icons/hi2";
 import { IoSettingsOutline } from "react-icons/io5";
 import { BiLogOut } from "react-icons/bi";
+import { BsShop } from "react-icons/bs";
 
 
 const Sidebar = () => {
     return (
         <>
-            <div className="flex flex-col items-start gap-5 bg-blue-900 w-1/5 h-screen p-10 text-white text-lg">
-                <h4 className="font-bold">Ecommerse Admin</h4>
-                <ul className="flex flex-col gap-5">
+            <div className="flex flex-col items-start gap-10 bg-blue-900 w-1/5 h-screen p-10 text-white text-lg">
+                <h4 className="flex items-center gap-4 font-bold">
+                <BsShop />Ecommerse Admin</h4>
+                <ul className="flex flex-col gap-8">
                     <li>
                         <Link href={'/'} className="hover:text-gray-300 text-md flex items-center gap-3">
                             <RxDashboard />
