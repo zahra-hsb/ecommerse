1616206d7677c309808024ef44a3d773584450cd
zahra <zahra20hb@gmail.com>
2024-02-09 00:42:42 +0330

    pathnames created with active links styles

--- pages/components/Sidebar/index.jsx
+++ pages/components/Sidebar/index.jsx
@@ -7,11 +7,18 @@ import { HiOutlineUserGroup } from "react-icons/hi2";
 import { IoSettingsOutline } from "react-icons/io5";
 import { BiLogOut } from "react-icons/bi";
 import { BsShop } from "react-icons/bs";
+import { useRouter } from "next/router";
 
 
 const Sidebar = () => {
     const inactiveLinks = 'text-white bg-blue-900'
     const activeLink = 'text-blue-900 bg-[#f9fafb] w-full p-3 hover:text-red-500 rounded-s-md'
+
+    const router = useRouter();
+
+    const {pathname} = router;
+
+
     return (
         <>
             <div className="flex flex-col items-start gap-10 bg-blue-900 w-1/5 h-screen p-10 text-white text-lg pr-0">
@@ -19,37 +26,37 @@ const Sidebar = () => {
                 <BsShop />Ecommerse Admin</h4>
                 <ul className="flex flex-col gap-8 w-full">
                     <li>
-                        <Link href={'/'} className={`${activeLink} hover:text-gray-300 text-md flex items-center gap-3`}>
+                        <Link href={'/'} className={`${pathname === '/' ? activeLink : inactiveLinks} hover:text-gray-300 text-md flex items-center gap-3`}>
                             <RxDashboard />
                             Dashboard
                         </Link>
                     </li>
                     <li>
-                        <Link href={'/products'} className={`${inactiveLinks} hover:text-gray-300 text-md flex items-center gap-3`}>
+                        <Link href={'/products'} className={`${pathname.includes('/products') ? activeLink : inactiveLinks} hover:text-gray-300 text-md flex items-center gap-3`}>
                             <VscArchive />
                             Products
                         </Link>
                     </li>
                     <li>
-                        <Link href={'/orders'} className={`${inactiveLinks} hover:text-gray-300 text-md flex items-center gap-3`}>
+                        <Link href={'/orders'} className={`${pathname.includes('/orders') ? activeLink : inactiveLinks} hover:text-gray-300 text-md flex items-center gap-3`}>
                             <GoNote />
                             Orders
                         </Link>
                     </li>
                     <li>
-                        <Link href={'/categories'} className={`${inactiveLinks} hover:text-gray-300 text-md flex items-center gap-3`}>
+                        <Link href={'/categories'} className={`${pathname.includes('/categories') ? activeLink : inactiveLinks} hover:text-gray-300 text-md flex items-center gap-3`}>
                             <RiListUnordered />
                             Categories
                         </Link>
                     </li>
                     <li>
-                        <Link href={'/admins'} className={`${inactiveLinks} hover:text-gray-300 text-md flex items-center gap-3`}>
+                        <Link href={'/admins'} className={`${pathname.includes('/admins') ? activeLink : inactiveLinks} hover:text-gray-300 text-md flex items-center gap-3`}>
                             <HiOutlineUserGroup />
                             Admins
                         </Link>
                     </li>
                     <li>
-                        <Link href={'/settings'} className={`${inactiveLinks} hover:text-gray-300 text-md flex items-center gap-3`}>
+                        <Link href={'/settings'} className={`${pathname.includes('/settings') ? activeLink : inactiveLinks} hover:text-gray-300 text-md flex items-center gap-3`}>
                         <IoSettingsOutline />
                             Settings
                         </Link>
