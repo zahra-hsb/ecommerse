10889f23d291b0f47f0e0ffea00ee5a02674fc2c
zahra <zahra20hb@gmail.com>
2024-02-10 02:38:55 +0330

    upload files codes added and created

--- pages/components/ProductForm/index.jsx
+++ pages/components/ProductForm/index.jsx
@@ -1,12 +1,14 @@
 import { Router, useRouter } from "next/router";
 import axios from "axios";
 import { useState } from "react";
+import { FiUpload } from "react-icons/fi";
 
 const ProductForm = ({
     _id,
     title: existingTitle,
     description: existingDescription,
-    price: existingPrice
+    price: existingPrice,
+    images,
 }) => {
     const [title, setTitle] = useState(existingTitle || '')
     const [description, setDescription] = useState(existingDescription || '')
@@ -20,7 +22,7 @@ const ProductForm = ({
         const data = { title, description, price };
 
         if (_id) {
-            await axios.put('/api/products', {...data, _id})
+            await axios.put('/api/products', { ...data, _id })
         } else {
             await axios.post('/api/products', data);
         }
@@ -28,6 +30,18 @@ const ProductForm = ({
     }
     if (goToProducts) {
         router.push('/products')
+    }
+    async function uploadImages(e) {
+        const files = e.target?.files;
+        if(files.length > 0) {
+            const data = new FormData();
+            for(const file of files) {
+                data.append('file', file)
+                
+            }
+            const res = await axios.post('/api/upload', data)
+            
+        }
     }
     return (
         <form onSubmit={saveProduct} className=" gap-5 p-5 flex flex-col items-start justify-start">
@@ -38,6 +52,17 @@ const ProductForm = ({
             <div className="w-full flex flex-col gap-2">
                 <label>Description</label>
                 <textarea placeholder="description" name="description" value={description} onChange={e => setDescription(e.target.value)}></textarea>
+            </div>
+            <div>
+                <label className='w-24 h-24 flex items-center justify-center border-2 cursor-pointer hover:bg-gray-300'>
+                    <FiUpload size={25} />
+                    <input type="file" onChange={uploadImages} className="hidden" />
+                </label>
+                {!images?.length && (
+                    <>
+                        <div>no photos in this product!</div>
+                    </>
+                )}
             </div>
             <div className="w-full flex flex-col gap-2">
                 <label>Price (in USA)</label>
