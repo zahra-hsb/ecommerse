3e0ca0859d8554cd87291b935850a718de535773
zahra <zahra20hb@gmail.com>
2024-02-10 02:39:12 +0330

    upload files codes added and created

---
+++ pages/api/upload.js
@@ -0,0 +1,18 @@
+import multiparty from 'multiparty'
+import { resolve } from 'styled-jsx/css'
+
+export default async function handle(req, res) {
+    const form = new multiparty.Form()
+    const {fields, files} = await new Promise((resolve, reject) => {
+        form.parse(req, (err, fields, files) => {
+            if(err) reject;
+            resolve({fields, files})
+        })
+    })
+    console.log(files.length)
+    return res.json('ok')
+}
+
+export const config = {
+    api: { bodyParser: false },
+}
