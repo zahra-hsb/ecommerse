23bf70402dc8bd1502d263f9af046de0f2701271
zahra <zahra20hb@gmail.com>
2024-02-11 23:23:32 +0330

    draggable images

--- pages/components/ProductForm/index.jsx
+++ pages/components/ProductForm/index.jsx
@@ -3,7 +3,8 @@ import axios from "axios";
 import { useState } from "react";
 import { FiUpload } from "react-icons/fi";
 import Image from "next/image";
-
+import Spinner from "../Spinner";
+import { ReactSortable } from "react-sortablejs";
 
 const ProductForm = ({
     _id,
@@ -17,6 +18,7 @@ const ProductForm = ({
     const [description, setDescription] = useState(existingDescription || '')
     const [price, setPrice] = useState(existingPrice || '')
     const [goToProducts, setGoToProducts] = useState('')
+    const [isUploading, setIsUploading] = useState(false)
     const router = useRouter()
 
 
@@ -36,11 +38,12 @@ const ProductForm = ({
     }
     async function uploadImages(e) {
         const files = e.target?.files;
-        if(files?.length > 0) {
+        if (files?.length > 0) {
+            setIsUploading(true)
             const data = new FormData();
-            for(const file of files) {
+            for (const file of files) {
                 data.append('file', file)
-                
+
             }
             const res = await axios.post('/api/upload', data)
             // console.log(res)
@@ -48,6 +51,10 @@ const ProductForm = ({
                 return [...oldImages, ...res.data.Links]
             })
         }
+        setIsUploading(false)
+    }
+    function uploadImagesOrder(images) {
+        setImages(images)
     }
     return (
         <form onSubmit={saveProduct} className=" gap-5 p-5 flex flex-col items-start justify-start">
@@ -59,23 +66,28 @@ const ProductForm = ({
                 <label>Description</label>
                 <textarea placeholder="description" name="description" value={description} onChange={e => setDescription(e.target.value)}></textarea>
             </div>
-            <div className="flex gap-5 overflow-x-scroll w-full">
-                {!!images?.length && images.map(link => (
+            <div className="flex overflow-x-scroll w-full">
+                <ReactSortable list={images} className="flex gap-5 p-5" setList={uploadImagesOrder}>
+                    {!!images?.length && images.map(link => (
+                        <>
+                            <div className="w-[150px] h-[180px] overflow-hidden rounded" key={link}>
+                                <img src={`${link}`} alt='sa' width={150} height={150} />
+                            </div>
+                        </>
+                    ))}
+                </ReactSortable>
+                {isUploading && (
                     <>
-                        <div className="w-1/4" key={link}>
-                            <img src={`${link}`} alt='sa' width={150} height={150}/>
+                        <div className="h-24">
+                            <Spinner />
                         </div>
                     </>
-                ))}
+                )}
                 <label className='w-[150px] h-[150px] flex items-center justify-center border-2 cursor-pointer hover:bg-gray-300'>
                     <FiUpload size={25} />
                     <input type="file" onChange={uploadImages} className="hidden" />
                 </label>
-                {!images?.length && (
-                    <>
-                        <div>no photos in this product!</div>
-                    </>
-                )}
+
             </div>
             <div className="w-full flex flex-col gap-2">
                 <label>Price (in USA)</label>
---
+++ pages/components/Spinner/index.js
@@ -0,0 +1,5 @@
+import { BounceLoader } from "react-spinners";
+
+export default function Spinner() {
+    <BounceLoader color="#000000" />
+}
