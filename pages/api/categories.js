8b733fa8e1155c92ad7c8db20df247e7ef935c5e
zahra <zahra20hb@gmail.com>
2024-05-18 14:57:54 -0700

    show categories  in categories page created and some styles added

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -2,11 +2,13 @@ import { Category } from "@/models/categories";
 
 export default async function handle(req, res) {
     const method = req.method
-
+    
     if (method === 'POST') {
         const { name } = req.body;
         const categoryDoc = await Category.create({name})
         res.json(categoryDoc)
+    } else if (method === 'GET') {
+        res.json(await Category.find())
     }
 
 }
--- pages/categories.js
+++ pages/categories.js
@@ -1,10 +1,10 @@
 import axios from "axios"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const { default: Layout } = require("./Layout")
 
 const Categories = () => {
-
+    const [category, setCategory] = useState([])
     const [name, setName] = useState('')
 
     function changeHandler(e) {
@@ -12,21 +12,32 @@ const Categories = () => {
     }
     async function saveCategory(e) {
         e.preventDefault()
-        await axios.post('/api/categories/', {name});
+        await axios.post('/api/categories/', { name });
         setName('')
     }
+
+    useEffect(() => {
+        axios.get('/api/categories').then(res => setCategory(res.data))
+    }, [])
     return (
         <>
             <Layout>
-                <main className="flex items-start flex-col w-full ">
-                    <h1>
+                <main className="flex items-start justify-start flex-col w-full h-full p-10">
+                    <h1 className="text-xl font-bold py-5">
                         Categories
                     </h1>
-                    <label>New category name</label>
+                    <label className="my-2">New category name</label>
                     <form onSubmit={(e) => saveCategory(e)} className="flex gap-1">
                         <input onChange={(e) => changeHandler(e)} value={name} type="text" placeholder="category name" />
                         <button type='submit' className="btn-primary" >Save</button>
                     </form>
+                    <ul>
+                        {category.map(cat => (
+                            <>
+                                <li>{cat.name}</li>
+                            </>
+                        ))}
+                    </ul>
                 </main>
 
             </Layout>
